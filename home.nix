0f4a29683eebbf8cb81bda8b422903b4cce50918
0x0003 <0x0003@users.noreply.github.com>
2026-06-27 20:51:28 +0900

    home: minimize locales

--- home.nix
+++ home.nix
@@ -1,7 +1,16 @@
-{ lib, username, homeDirectory, ... }: {
+{ lib, pkgs, username, homeDirectory, ... }: {
   home = {
     inherit username homeDirectory;
     stateVersion = "24.11";
+  };
+
+  i18n.glibcLocales = pkgs.glibcLocales.override {
+    allLocales = false;
+    locales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+    ];
   };
 
   programs.home-manager.enable = true;
