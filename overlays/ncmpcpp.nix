d5c3bfeb5bb02d8830f9a9a0c9660c6717ac9722
0x0003 <0x0003@users.noreply.github.com>
2026-07-18 19:47:05 +0900

    ncmpcpp: add escape abort patch

--- overlays/ncmpcpp.nix
+++ overlays/ncmpcpp.nix
@@ -4,6 +4,7 @@ _: prev: {
       ./../patches/ncmpcpp-single-line-header.patch
       ./../patches/ncmpcpp-sticker-rating.patch
       ./../patches/ncmpcpp-column-truncation-symbol.patch
+      ./../patches/ncmpcpp-escape-abort-prompt.patch
     ];
     NIX_BUILD_CORES = 4;
   });
